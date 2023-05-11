9c0b5bf241e98a1d03fadbd819dfae36127dd79f
QDat <92576462+muradmtp5555@users.noreply.github.com>
2023-05-11 17:28:18 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,43 @@
+let darkModeIcon = document.querySelector("#darkMode-icon");
+
+darkModeIcon.onclick = () => {
+  darkModeIcon.classList.toggle("bx-sun");
+  document.body.classList.toggle("dark-mode");
+};
+
+const active = document.querySelector("#active");
+const menu = document.querySelector("nav");
+
+menu.addEventListener("click", (e) => {
+  if ((e.target.nodeName = "A")) {
+    active.style.left = e.target.offsetLeft + "px";
+    active.style.width = e.target.offsetWidth + "px";
+    active.style.display = "block";
+  }
+});
+
+const logoclick = document.querySelector(".logo-click");
+
+logoclick.onclick = function () {
+  active.style.display = "none";
+};
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 6000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+//----------------------------------------------------------------------------------------------
